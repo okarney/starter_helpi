ecc80991323acf9d27a6d47d70411c56b65c060c
JacobW <jacobwhi@udel.edu>
2024-04-11 14:28:36 -0400

    aAAAAAAAAAAAAAA

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@
 //import logo from './logo.svg';
 import './App.css';
 //import { Button, Form } from 'react-bootstrap';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routerer from './Routerer';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      
         <Routerer />
-      </BrowserRouter>
+      
     </div>
   );
 
--- src/Routerer.tsx
+++ src/Routerer.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Routes, Route }
+import { BrowserRouter as Routes, Route, BrowserRouter }
 
     from "react-router-dom";
 //import App from "./App";
@@ -11,13 +11,13 @@ import Home from "./HomeQuestions";
 function Routerer() {
     return (
         <div>
-            
+            <BrowserRouter>
             <Routes>
                 <Route path="/basic" element={<Basic />} />
                 <Route path="/detailed" element={<Detailed />} />
                 <Route path="/" element={<Home />} /> {/* Assuming Home is your landing page */}
             </Routes>
-            
+            </BrowserRouter>
         </div>
     );
 }
