dc6ebc057b9594920777633d54507c9b3ada8a41
Olivia <okarney@udel.edu>
2024-04-11 19:45:51 -0400

    saved basic

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button, Form, ProgressBar } from 'react-bootstrap';
+import { Button, Form} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { BasicExample } from './progressBar';
 
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function App(){
   return <span>There is actually something here</span>
 }
