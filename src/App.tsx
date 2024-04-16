642e821ee709bfe1175644cf7f0e94c17a3b3a00
JacobW <jacobwhi@udel.edu>
2024-04-16 16:15:48 -0400

    Made Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
 import './router';
+// import BasicQuestions from "./BasicQuestions";
+// import DetailedQuestions from './DetailedQuestions';
+import { Link } from "react-router-dom";
 
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
@@ -14,33 +17,25 @@ if (prevKey !== null) {
 }
 
 function BasicQuestions() {
-  const navigate = useNavigate();
 
-  function handleClick2() {
-    navigate('/BasicQuestions');
-  }
 
   return (
-    <button type="button" onClick={handleClick2}>
-    Basic Assessment
-    </button>
+    <Link to="/BasicQuestions">
+        <Button className="BasicButton">Basic Questions</Button>
+      </Link>
   );
 }
 
 function DetailedQuestions() {
-  const navigate = useNavigate();
 
-  function handleClick1() {
-    navigate('/DetailedQuestions');
-  }
 
   return (
-    <button
-    type="button"
-    onClick={handleClick1}
-    style={{ marginLeft: '20px' }}>
-    Detailed Assessment
-    </button>
+    <Link to="/DetailedQuestions">
+      <Button className="DetailedButton">
+      Detailed Assessment
+      </Button>
+    </Link>
+    
   );
 }
 
@@ -48,17 +43,6 @@ function DetailedQuestions() {
 
 function App() {
     
- 
-    // const goToDetailed = () => {
- 
-    //     // This will navigate to second component
-    //     Nav('/DetailedQuestions');
-    // };
-    // const gotToBasic = () => {
- 
-    //     // This will navigate to first component
-    //     Nav('/BasicQuestions');
-    // };
   const [key, setKey] = useState<string>(keyData); //for api key input
   
   //sets the local storage item to the api key the user inputed
@@ -76,30 +60,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The Career Helpi</h1>
-        
-        {/*<p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Our Names</h1>
+        <br></br>
+        <span>Olivia Karney</span>
+        <span>Jacob Whitman</span>
+        <span>Zahra Temori</span>
+        <span>Khadija Mohammadi</span>
+        <br></br>
+        {/*Insert Next Name Below!!!*/}
+        <p>
           <div style={{ margin: '30px 0' }}>
             
 
+            <BasicQuestions/>
+            <p style={{ fontSize: '13px' }}> Discover your potential career path through our refined multiple-choice assessment.</p>
+            <DetailedQuestions/>
+            <p style={{ fontSize: '13px' }}>Discover your potential career path through our comprehensive assessment featuring detailed questions.</p>
+
+
           </div>
-  </p>*/}
-
-        <Container>
-                <Row>
-                    <Col>
-                    <BasicQuestions/>
-                    <p style={{ fontSize: '13px' }}> Discover your potential career path through our refined multiple-choice assessment.</p>
-                        
-                    </Col>
-                    <Col>
-                    <DetailedQuestions/>
-                    <p style={{ fontSize: '13px' }}>Discover your potential career path through our comprehensive assessment featuring detailed questions.</p>
-
-                    </Col>
-                </Row>
-            </Container>
-        
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
@@ -107,7 +96,6 @@ function App() {
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
-      <span>Created By: Olivia Karney, Khadija Mohammadi, Zahra Temori, and Jacob Whitman</span>
     </div>
   );
 }
