e430995eb4a182db3d7cae50b856dee980eec2ef
JacobW <jacobwhi@udel.edu>
2024-04-18 15:57:29 -0400

    a

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../App.css';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import '../router';
+import '../App';
 //import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
