2bb932ae1c8c806f6f652cd528f591fb74dd0384
JacobW <jacobwhi@udel.edu>
2024-04-23 16:09:58 -0400

    Removed-Keyfrom-Home

--- src/progressBar.tsx
+++ src/progressBar.tsx
@@ -5,7 +5,7 @@ interface Bar {
 export function BasicExample({progress}: Bar):JSX.Element {
   return (
     <div>
-    <h1 style = {{color: "#282c34", backgroundColor: "#282c34"}}>Progress Barrrrrrrrrrrrrrr</h1>
+    <h1 style = {{color: "#282c34", backgroundColor: "#282c34", width: "80%", justifyContent: 'center', alignItems: 'center'}}>Progress Barrrrrrrrrrrrrrr</h1>
     <ProgressBar now={progress}/> 
   </div>
   );
