9c21ee274cd3ddec7e0dca73cc98a9a1a9eff92c
JacobW <jacobwhi@udel.edu>
2024-04-30 17:09:57 -0400

    formatted the pages

--- src/progressBar.tsx
+++ src/progressBar.tsx
@@ -5,7 +5,7 @@ interface Bar {
 export function BasicExample({progress}: Bar):JSX.Element {
   return (
     <div>
-    <h1 style = {{color: "#ffffff", backgroundColor: "#ffffff", width: "80%", justifyContent: 'center', alignItems: 'center'}}>Progress Barrrrrrrrrrrrrrr</h1>
+    {/* <h1 style = {{color: "#ffffff", backgroundColor: "#ffffff", width: "80%", justifyContent: 'center', alignItems: 'center', fontSize: 0}}>Progress Barrrrrrrrrrrrrrr</h1> */}
     <ProgressBar now={progress} className='Progress-bar'/> 
   </div>
   );
